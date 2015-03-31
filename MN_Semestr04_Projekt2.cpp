8b423ac68d5345a855c58ec8d52f244b55bd50f2
milleniumbug <wojas1222@gmail.com>
2015-03-31 04:57:58 +0200

    dodano rozwiązywanie układów liniowych

--- MN_Semestr04_Projekt2.cpp
+++ MN_Semestr04_Projekt2.cpp
@@ -40,6 +40,18 @@ public:
 	int kolumny() const { return width; }
 };
 
+void wypisz_macierz(const Matrix& m)
+{
+	for(int i = 1; i <= m.wiersze(); ++i)
+	{
+		for(int j = 1; j <= m.kolumny(); ++j)
+		{
+			std::cout << std::setw(10) << m(i, j) << " ";
+		}
+		std::cout << "\n";
+	}
+}
+
 std::pair<Matrix, Matrix> dekompozycja_lu(const Matrix& a)
 {
 	assert(a.wiersze() == a.kolumny());
@@ -77,20 +89,39 @@ std::pair<Matrix, Matrix> dekompozycja_lu(const Matrix& a)
 	return ret;
 }
 
-void wypisz_macierz(const Matrix& m)
+Matrix rozwiaz_uklad(const Matrix& a, const Matrix& y)
 {
-	for(int i = 1; i <= m.wiersze(); ++i)
+	auto d = dekompozycja_lu(a);
+	Matrix& l = d.first;
+	Matrix& u = d.second;
+	Matrix z(l.kolumny(), 1);
+	Matrix x(l.kolumny(), 1);
+
+	// l * z = y
+	for(int i = 1; i <= l.wiersze(); ++i)
 	{
-		for(int j = 1; j <= m.kolumny(); ++j)
-		{
-			std::cout << std::setw(10) << m(i, j) << " ";
-		}
-		std::cout << "\n";
+		double suma = y[i];
+		for(int j = 1; j <= i - 1; ++j)
+			suma -= l(i, j)*z[j];
+		z[i] = suma / l(i,i);
 	}
+
+	// u * x = z
+	for(int i = l.wiersze(); i >= 1; --i)
+	{
+		double suma = z[i];
+		for(int j = i + 1; j <= l.wiersze(); ++j)
+			suma -= u(i, j)*x[j];
+		x[i] = suma / u(i, i);
+	}
+
+	return x;
 }
 
 int main()
 {
+	const int op = 2;
+	if(op == 0)
 	{
 		Matrix m(3, 2);
 		m(1, 1) = 1;
@@ -101,9 +132,7 @@ int main()
 		m(3, 2) = 6;
 		wypisz_macierz(m);
 	}
-
-	std::cout << "\n\n";
-
+	else if(op == 1)
 	{
 		Matrix m(3, 3);
 		m(1, 1) = 5;
@@ -119,5 +148,25 @@ int main()
 		auto d = dekompozycja_lu(m);
 		wypisz_macierz(d.first);
 		wypisz_macierz(d.second);
+	}
+	else if(op == 2)
+	{
+		Matrix m(3, 3);
+		m(1, 1) = 5;
+		m(1, 2) = 3;
+		m(1, 3) = 2;
+		m(2, 1) = 1;
+		m(2, 2) = 2;
+		m(2, 3) = 0;
+		m(3, 1) = 3;
+		m(3, 2) = 0;
+		m(3, 3) = 4;
+
+		Matrix y(1, 3);
+		y[1] = 10;
+		y[2] = 5;
+		y[3] = -2;
+
+		auto x = rozwiaz_uklad(m, y);
 	}
 }
