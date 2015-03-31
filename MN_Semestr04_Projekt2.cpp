a20c0027426479c88a4c98d937670441447bcb8f
milleniumbug <wojas1222@gmail.com>
2015-03-31 02:00:14 +0200

    dekompozycja LU, pierwsza wersja

--- MN_Semestr04_Projekt2.cpp
+++ MN_Semestr04_Projekt2.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <utility>
 #include <iostream>
+#include <iomanip>
 #include <cassert>
 
 class Matrix
@@ -29,21 +30,84 @@ public:
 	int kolumny() const { return width; }
 };
 
-int main()
+std::pair<Matrix, Matrix> dekompozycja_lu(const Matrix& a)
 {
-	Matrix m(3, 2);
-	m(1, 1) = 1;
-	m(1, 2) = 2;
-	m(2, 1) = 3;
-	m(2, 2) = 4;
-	m(3, 1) = 5;
-	m(3, 2) = 6;
+	assert(a.wiersze() == a.kolumny());
+	int n = a.wiersze();
+	std::pair<Matrix, Matrix> ret(Matrix(n, n), Matrix(n, n));
+	Matrix& l = ret.first;
+	Matrix& u = ret.second;
+
+	// wype³nij jedynkami przek¹tn¹ macierzy L
+	for(int i = 1; i <= n; ++i)
+		l(i, i) = 1;
+
+	for(int i = 1; i <= n; ++i)
+	{
+		for(int j = i; j <= n; ++j)
+		{
+			double suma = 0;
+			for(int k = 1; k <= i - 1; ++k)
+			{
+				suma += l(i, k)*u(k, j);
+			}
+			u(i, j) = a(i, j) - suma;
+		}
+		for(int j = i + 1; j <= n; ++j)
+		{
+			double suma = 0;
+			assert(u(i, i) != 0);
+			for(int k = 1; k <= i - 1; ++k)
+			{
+				suma += l(j, k)*u(k, i);
+			}
+			l(j, i) = (a(j, i) - suma) / u(i, i);
+		}
+	}
+	return ret;
+}
+
+void wypisz_macierz(const Matrix& m)
+{
 	for(int i = 1; i <= m.wiersze(); ++i)
 	{
 		for(int j = 1; j <= m.kolumny(); ++j)
 		{
-			std::cout << m(i, j) << " ";
+			std::cout << std::setw(10) << m(i, j) << " ";
 		}
 		std::cout << "\n";
+	}
+}
+
+int main()
+{
+	{
+		Matrix m(3, 2);
+		m(1, 1) = 1;
+		m(1, 2) = 2;
+		m(2, 1) = 3;
+		m(2, 2) = 4;
+		m(3, 1) = 5;
+		m(3, 2) = 6;
+		wypisz_macierz(m);
+	}
+
+	std::cout << "\n\n";
+
+	{
+		Matrix m(3, 3);
+		m(1, 1) = 5;
+		m(1, 2) = 3;
+		m(1, 3) = 2;
+		m(2, 1) = 1;
+		m(2, 2) = 2;
+		m(2, 3) = 0;
+		m(3, 1) = 3;
+		m(3, 2) = 0;
+		m(3, 3) = 4;
+		wypisz_macierz(m);
+		auto d = dekompozycja_lu(m);
+		wypisz_macierz(d.first);
+		wypisz_macierz(d.second);
 	}
 }
